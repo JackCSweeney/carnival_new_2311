6b040a4e8ed6860eec0f7bb855f0bec97cb07963
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-09 08:28:53 -0800

    test: Add tests for board_rider to Ride

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -40,4 +40,34 @@ RSpec.describe Ride do
             expect(ride1.rider_log).to eq({})
         end
     end
+
+    describe '#board_rider(rider)' do
+        it 'can board a rider and add it to its rider log' do
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 36, '$5')
+
+            ride1.board_rider(visitor1)
+
+            expect(ride1.rider_log).to eq({visitor1 => 1})
+
+            ride1.board_rider(visitor2)
+            ride1.board_rider(visitor1)
+
+            expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+        end
+
+        it 'charges the rider the admission fee when boarding the rider' do
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 36, '$5')
+
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor2)
+
+            expect(visitor1.spending_money).to eq(8)
+            expect(visitor2.spending_money).to eq(4)
+        end
+    end
 end
