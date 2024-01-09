44686db8d068ff07d0dc0986a90d9dc96af3ded6
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-09 08:59:45 -0800

    test: Add tests for initialize to Carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,1 +1,26 @@
 require 'spec_helper'
+require './lib/visitor'
+require './lib/ride'
+require './lib/carnival'
+
+RSpec.describe Carnival do
+    describe '#initialize' do
+        it 'exists' do
+            carnival = Carnival.new(4)
+
+            expect(carnival).to be_a(Carnival)
+        end
+
+        it 'has a duration' do
+            carnival = Carnival.new(4)
+
+            expect(carnival.duration).to eq(4)
+        end
+
+        it 'starts with no rides' do
+            carnival = Carnival.new(4)
+
+            expect(carnival.rides).to eq([])
+        end
+    end
+end
