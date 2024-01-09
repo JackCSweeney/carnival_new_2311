a8c8efc30861f6731fc6fe1a785066ccffdc3a84
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-09 09:29:31 -0800

    test: Add test for summary to Carnival

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -121,4 +121,78 @@ RSpec.describe Carnival do
             
         end
     end
+
+    describe '#summary' do
+        before(:each) do
+            @carnival = Carnival.new(4)
+
+            @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+            @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+            @carnival.add_ride(@ride1)
+            @carnival.add_ride(@ride2)
+            @carnival.add_ride(@ride3)
+
+            @visitor1 = Visitor.new('Bruce', 54, '$10')
+            @visitor2 = Visitor.new('Tucker', 36, '$6')
+            @visitor3 = Visitor.new('Penny', 64, '$15')
+
+            @visitor1.add_preference(:gentle)
+            @visitor2.add_preference(:gentle)
+            @visitor2.add_preference(:thrilling)
+            @visitor3.add_preference(:thrilling)
+
+            @ride1.board_rider(@visitor1) 
+            @ride1.board_rider(@visitor1) 
+            @ride1.board_rider(@visitor2) 
+            @ride2.board_rider(@visitor1) 
+            @ride2.board_rider(@visitor2) 
+            @ride3.board_rider(@visitor3) 
+            @ride3.board_rider(@visitor3) 
+            @ride3.board_rider(@visitor3) 
+            @ride3.board_rider(@visitor3) 
+        end
+
+        it 'can return a summary of the carnival' do
+            expect(carnival.summary).to eq({
+                visitor_count: 3, 
+                revenue_earned: 21, 
+                visitors: [
+                  {
+                    visitor: @visitor1,
+                    favorite_ride: @ride1,
+                    total_money_spent: 7
+                  }, 
+                  {
+                    visitor: @visitor2,
+                    favorite_ride: @ride1,
+                    total_money_spent: 6
+                  },
+                  {
+                    visitor: @visitor3,
+                    favorite_ride: @ride3,
+                    total_money_spent: 8
+                  }],
+                rides: [
+                  {
+                    ride: @ride1,
+                    riders: [@visitor1, @visitor2],
+                    total_revenue: 3
+                  },
+                  {
+                    ride: @ride2,
+                    riders: [@visitor1, @visitor2],
+                    total_revenue: 10
+                  },
+                  {
+                    ride: @ride3,
+                    riders: [@visitor3],
+                    total_revenue: 8
+                  }]
+                })
+            end
+        end
+
+
 end
