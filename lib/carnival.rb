cf419c8f7c1191d06eb8d05ff8b3b913de297baf
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-09 09:13:55 -0800

    feat: Add add_ride, most_popular_ride, and most_profitable_ride to Carnival

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -7,4 +7,16 @@ class Carnival
         @duration = duration
         @rides = []
     end
+
+    def add_ride(ride)
+        @rides << ride
+    end
+
+    def most_popular_ride
+        @rides.max_by {|ride| ride.rider_log.values.sum}
+    end
+
+    def most_profitable_ride
+        @rides.max_by {|ride| ride.total_revenue}
+    end
 end
